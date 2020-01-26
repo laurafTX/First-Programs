db50a4148d29aeed7fc6a4e2441c0341a56d08cb
Laura Finger <58643281+laurafTX@users.noreply.github.com>
2020-01-26 00:41:43 -0600

    Longer version of Magic 8 ball
    This is the first method Automate the Boring Stuff With Python used to show me how to do the Magic 8 Ball game.  As far as I knew, it was the only way to do it at the time.  I enjoyed using the elif tags.

---
+++ Magig8ball.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 24 04:23:43 2020
+
+@author: laurafinger
+"""
+
+import random
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return 'It is certain'
+    elif answerNumber == 2:
+        return 'It is decidedly so'
+    elif answerNumber == 3:
+        return 'Yes'
+    elif answerNumber == 4:
+        return 'Reply hazy try again'
+    elif answerNumber == 5:
+        return 'Ask again later'
+    elif answerNumber == 6:
+        return 'Concentrate and ask again'
+    elif answerNumber == 7:
+        return 'My reply is no'
+    elif answerNumber == 8:
+        return 'Outlook not so good'
+    elif answerNumber == 9:
+        return 'Very doubtful'
+    
+
+print(getAnswer(random.randint(1, 9)))  
